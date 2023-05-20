e328831f920b903cf6d3451c9a91f9bc0bb76312
asudakova <thesashek@yandex.ru>
2023-05-20 22:40:16 +0300

    Fix unused import

--- src/pages/PlaceInfoPage/PlaceInfoPage.tsx
+++ src/pages/PlaceInfoPage/PlaceInfoPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './PlaceInfoPage.module.css';
 import { useParams } from 'react-router-dom';
 import { useAppSelector } from '../../redux/typingReduxHooks';
